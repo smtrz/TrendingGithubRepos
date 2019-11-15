c57919bb10d3c83f9f2b502d30aa1d69e69e0a48
Tahir Raza <smtrz@yahoo.com>
2019-11-16 03:36:44 +0500

    - Toolbar design completed - Design for the Cell for trending repositories completed.

---
+++ app/src/main/java/com/tahir/go_jek/Activities/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.tahir.go_jek.Activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import com.tahir.go_jek.R
+
+
+open class BaseActivity : AppCompatActivity() {
+    // All the common functions will override here //
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+      //  val inflater = menuInflater
+        //inflater.inflate(R.menu.manu, item)
+        return true
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+}
---
+++ app/src/main/java/com/tahir/go_jek/Activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tahir.go_jek
+
+import android.os.Bundle
+import com.tahir.go_jek.Activities.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Attaching the layout to the toolbar object
+        //setting toolbar
+       // setSupportActionBar(findViewById(R.id.tool_bar))
+
+    }
+}
